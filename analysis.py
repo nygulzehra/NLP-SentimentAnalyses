1b9d2f435ea7ea623945f27df3db09a18c5404d6
Zehra Gul C <108553856+nygulzehra@users.noreply.github.com>
2023-05-05 21:58:08 -0400

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,51 @@
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+from joblib import load
+
+def expression_check(prediction_input):
+
+    if prediction_input == -1:
+        print("👎🏻 Negative Sentiment.")
+    elif prediction_input == 1:
+        print("👍🏻 Positive Sentiment.")
+    else:
+        print("Invalid Statement.")
+
+# function to take the input statement and perform the same transformations we did earlier
+def sentiment_predictor(input):
+    input = text_transformation(input)
+
+    lr1 = load('Desktop/DataScience/proje4 - NLP/streamlit/lr1.pkl')
+    tfidf1 = load("/Users/zehragul/Desktop/DataScience/proje4 - NLP/streamlit/tfidf.pkl")
+
+    transformed_input=tfidf1.transform(input)
+    prediction = lr1.predict(transformed_input)
+    expression_check(prediction)
+
+# Sayfa Ayarları
+st.set_page_config(
+    page_title="NlP - Sentiment Analysis",
+    page_icon="",
+    layout="centered",
+    menu_items={
+        "Get help": "mailto:nygulzehra@gmail.com",
+        "About": "For More Information\n" + "https://github.com/nygulzehra"
+    }
+)
+
+st.title("**black[Review Sentiment Analysis Project]**")
+st.image("/Users/zehragul/Desktop/DataScience/proje4 - NLP/streamlit/pos-neg.png")
+st.markdown("Let's try ! ")
+
+st.subheader("*:blue[Let's try sentiment analysis using machine learning!]*")
+text = st.text_input("Write here )
+
+st.button("Submit")
+    #---------------------------------------------------------------------------------------------------------------------
+    # Sonuç Ekranı
+if st.button("Submit"):
+
+    st.info("*result*")
+    sentiment_predictor(text)
